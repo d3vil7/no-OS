7bd59c83a0bc7c28f2a1ba14f87ed1bb0d0f0e43
Cristian Pop <cristian.pop@analog.com>
2019-02-26 16:30:40 +0200

    ad9361: switch to common drivers
    switch to axi_adc_core, axi_dac_core, axi_dmac interface
    
    Signed-off-by: Cristian Pop <cristian.pop@analog.com>

--- projects/ad9361/src/ad9361.c
+++ projects/ad9361/src/ad9361.c
@@ -39,6 +39,7 @@
 /******************************************************************************/
 /***************************** Include Files **********************************/
 /******************************************************************************/
+#include <ad9361_util.h>
 #include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -46,7 +47,7 @@
 #include <string.h>
 #include <inttypes.h>
 #include "ad9361.h"
-#include "platform.h"
+#include "platform_drivers.h"
 #include "util.h"
 #include "config.h"
 
